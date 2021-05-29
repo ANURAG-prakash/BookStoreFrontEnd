2ace0cb3e5808d7bf42fd79d3ad73ca370b32ac0
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-05-29 07:43:19 +0530

    Api integration is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-
+import { BrowserRouter, Route, Link , Switch, Redirect} from 'react-router-dom';
 import './App.css';
 import Dashboard from './pages/loginsignupdashboard';
+import BSDashboard from './pages/BookStoreDashBoard';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+    <Switch>
+      <Route exact path= "/LoginSignUp" component={Dashboard} />
+      <Route exact path= "/Dashboard" component={BSDashboard} />
       
-      <Dashboard/>
-
-    </div>
+    </Switch>
+  </BrowserRouter>
+   
   );
 }
 
--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -3,12 +3,80 @@ import { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import './login.css';
+import Userservice from '../../services/userservices';
 
-export default class loginbox extends React.Component {
+
+
+
+const axios_service = new Userservice();
+
+interface IProps {
+}
+
+interface loginboxState {
+    email: string,
+    password: string,
+    emailerror: boolean,
+    passworderror: boolean, 
+    redirect: any,
+    open: boolean
+}
+
+
+export default class loginbox extends React.Component<{}, loginboxState> {
     constructor(props: any) {
         super(props)
-        this.state = {}
+        this.state = {
+            email: '',
+            password: '',
+            emailerror: false,
+            passworderror: false,
+            redirect: null,
+            open: false
+        }
     }
+   
+
+      validation = () =>{
+        let isError : boolean = false;
+        const errors : any = this.state;
+        errors.emailerror = this.state.email === '' ? true : false;
+        errors.passworderror = this.state.password === '' ? true : false;
+        this.setState({
+            ...errors,
+            // ...this.state
+        })
+        return isError = (errors.email!=='' && errors.password!=='' ) ? true : false
+    }
+     
+    Next = () => {
+
+        var isValidated = this.validation();
+        console.log(this.state.email);
+        console.log(this.state.password);
+    
+        if (isValidated) {
+          this.setState({ open: true });
+          let data = {
+            "email": this.state.email,
+            "password": this.state.password
+          };
+    
+          console.log("validation successful");
+          axios_service.Login(data).then((result) => {
+            console.log(result);
+            this.setState({ open: true });
+            localStorage.setItem('id',result.data.token);
+            setTimeout(() => this.setState({ redirect: "/bookStore" }), 4000)
+    
+          }).catch(() => {
+    
+          })
+    
+        }
+    }
+
+
     render() {
         return (
             <div className="inner-container">
@@ -38,7 +106,7 @@ export default class loginbox extends React.Component {
                         </div>
 
                         <div className="Button">
-                            <Button variant="contained" color="secondary" fullWidth >  Log IN </Button>
+                            <Button variant="contained" color="secondary" onClick={this.Next} >  Log IN </Button>
                         </div>
 
                         <div className="Or-Tag">
