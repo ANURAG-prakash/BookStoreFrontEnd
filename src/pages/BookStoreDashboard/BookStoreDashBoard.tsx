594a8918876bde168b193038d6d3f13ae2d16530
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-05-31 02:46:19 +0530

    BookStore dashboard is added

---
+++ src/pages/BookStoreDashboard/BookStoreDashBoard.tsx
@@ -0,0 +1,196 @@
+import React, { Component } from 'react'
+
+import './BookStore.css'
+
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import MenuBookSharpIcon from '@material-ui/icons/MenuBookSharp';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import Button from '@material-ui/core/Button';
+
+import { Redirect } from "react-router-dom";
+
+import Userservice from '../../services/userservices';
+
+import book1 from '../../assert/book1.png';
+
+const axios_service = new Userservice();
+
+
+
+interface BookStoreState {
+    notes: any,
+    redirect: any,
+    openDropDown : boolean
+}
+
+export default class BookStore extends Component<BookStoreState> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            notes: [],
+            redirect: null,
+            openDropDown: false
+        }
+
+    }
+
+    componentDidMount() {
+        this.GetData();
+    }
+
+    GetData = () => {
+        axios_service.Getdata().then((result) => {
+            console.log(result.data.books);
+            this.setState({ notes: result.data.books });
+            console.log(this.state.notes);
+            console.log(this.state.notes.bookName[0]);
+        }).catch(() => {
+
+        })
+    }
+
+    addtoCart = (value : any) => {
+        console.log();
+        axios_service.AddtoCart(value).then((result) => {
+            console.log(result.data);
+            
+        }).catch(() => {
+
+        })
+    }
+    
+    addtoWishList = (value : any) => {
+        axios_service.AddtoWishList(value).then((result) => {
+            console.log(result.data);
+        }).catch(() => {
+
+        })
+    }
+
+    toWishList = () => {
+        this.setState({ redirect: "/bookStore/wishlist" });
+    }
+
+    toCart = () => {
+        this.setState({ redirect: "/bookStore/cart" });
+    }
+
+    opendropdown = () => {
+        this.setState({ openDropDown : true });
+    }
+
+    closedropdown = () => {
+        this.setState({ openDropDown : false });
+    }
+
+    render() {
+
+        if (this.state.redirect) {
+
+            return <Redirect to={this.state.redirect} />
+        }
+
+        return (
+
+            <div>
+                <header>
+
+                    <MenuBookSharpIcon className="MenuBookSharpIcon" />
+                    <div className="Name">Bookstore</div>
+
+                    <div className="inputbase">
+
+                        <div className="searchIcon"><SearchIcon /></div>
+                        <InputBase
+                            placeholder="Search"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </div>
+
+                    <div className="PersonOutlineOutlinedIcon">
+                        {this.state.openDropDown ? 
+                        <div className = "dropdown">
+                            <PersonOutlineOutlinedIcon onClick={this.closedropdown}/>
+                            <form>
+                            <select>
+                                <option>Hello Prashik</option>
+                                <option onClick = {this.toWishList}>WishList</option>
+                            </select>
+                            </form>
+                        </div>
+                        :
+                        <div>
+                        <PersonOutlineOutlinedIcon onClick={this.opendropdown}/>
+                        <div className="Style"> Person </div>
+                        </div>
+                        }
+                    </div>
+                    <div className="ShoppingCartIcon"><ShoppingCartIcon onClick={this.toCart} /> <div className="Style">Cart</div></div>
+
+                </header>
+                <div className="Body">
+                    <div className="Title">Books</div>
+
+
+                    <Grid item xs={12}>
+                        <Grid container justify="flex-start">
+                            {this.state.notes.slice(0).reverse().map((value: any) =>
+
+                                <Grid key={value.id} item >
+
+                                    <Paper className="paper">
+
+                                        <div>
+                                            <div className="img">
+                                                <img id = "image2" src={book1} alt="Book" />
+                                            </div>
+
+                                            <div className="Intro">
+
+                                                <div className="bookName">{value.bookName} </div>
+                                                <div className="by">by {value.authors}</div>
+                                                <div className="rating">4.5 <div className="number">({value.availableBooks})</div></div>
+                                                <div className="price">Rs.{value.price}</div>
+                                                <div className= "bookbuttons">
+
+                                                <div >
+                                                <Button className = "buttonsize" onClick = {() => this.addtoCart(value.id)} size = "small" variant="contained" color="secondary">
+                                                Add to Bag
+                                                </Button>
+                                                </div>
+                                                <div >
+                                                <Button className = "buttonsize" onClick = {() => this.addtoWishList(value.id)} size = "small" variant="contained">
+                                                WishList
+                                                </Button>
+                                                </div>
+
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    </Paper>
+
+                                </Grid>
+
+                            )}
+                        </Grid>
+                    </Grid>
+
+
+                </div>
+
+                <footer>
+                    <div className="text">
+                        Copyright 2020, BookStore Private Limited.All Rights Reserved
+                    </div>
+                </footer>
+            </div>
+        )
+    }
+}
