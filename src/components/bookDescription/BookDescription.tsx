36c59318dedcbb86f891141fcfc4e14ba9ce0e5b
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-06-10 07:58:58 +0530

    Book Description is done

--- src/components/bookDescription/BookDescription.tsx
+++ src/components/bookDescription/BookDescription.tsx
@@ -14,8 +14,7 @@ export class BookDescription extends Component {
         <div className="DetailsConatiner">
         A book is a medium for recording information in the form of writing or images,
          typically composed of many pages (made of papyrus, parchment, vellum, or paper) bound together and protected by a cove
-         r. The technical term for this physical arrangement is codex (plural, codices). In the history of hand-held physical supports for extended written compositions or records,
-          the codex replaces its predecessor, the scroll. A single sheet in a codex is a leaf and each side of a leaf is a page.
+         r. 
         </div>
         
       </div>
